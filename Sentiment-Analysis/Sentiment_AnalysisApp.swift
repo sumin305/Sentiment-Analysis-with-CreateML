a12e5ba7682b7e6af105cef650419c8c9264c637
sumin305 <gogotnals@naver.com>
2023-03-15 21:53:02 +0900

    Initial Commit

---
+++ Sentiment-Analysis/Sentiment_AnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sentiment_AnalysisApp.swift
+//  Sentiment-Analysis
+//
+//  Created by 이수민 on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct Sentiment_AnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
